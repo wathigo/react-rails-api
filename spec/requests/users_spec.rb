fc3a09f62025e3ee38f3ec0126a186e4ecdf59eb
wathigo <wathigosimon@gmail.com>
2020-01-17 02:38:50 -0500

    Update user's request test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'Users API', type: :request do
 
       it 'returns failure message' do
         expect(json['message'])
-          .to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+          .to match(/Validation failed: Name can't be blank, Password digest can't be blank, Email can't be blank, Email is invalid, Password can't be blank/)
       end
     end
   end
