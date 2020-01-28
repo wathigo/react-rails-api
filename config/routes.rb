002f68c8015bb037517519904c6e534c30af4740
wathigo <wathigosimon@gmail.com>
2020-01-28 10:22:02 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :categories do
     resources :properties
   end
+  root to: 'categories#index'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ['Housing Properties', 'Land Properties']
+categories.each do |n|
+  Category.create!(name: n)
+end
+
+categories = Category.all
+
+all_status = ['booked', 'reserved', 'bought']
+
+categories.each do |category|
+  8.times do |n|
+    category.properties.create!(
+      name: Faker::Name.unique.name,
+      address: Faker::Address.full_address,
+      description: Faker::Lorem.paragraph(sentence_count: 30),
+      price: (200000 + rand(1000000)),
+      latt: Faker::Address.latitude,
+      long: Faker::Address.longitude,
+      status: all_status.sample
+    )
+  end
+end
